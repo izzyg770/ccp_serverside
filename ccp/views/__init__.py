070b44fbea0ce112f1705fd9f8876f2acf1ca4c7
igiallan <igiallan@umich.edu>
2024-04-10 15:27:47 -0400

    added user implementation, followers, following

--- ccp/views/__init__.py
+++
@@ -1,8 +0,0 @@
-"""CCP REST API."""
-from ccp.views.index import show_index
-from ccp.views.pets import show_pets
-from ccp.views.recipes import show_recipes
-from ccp.views.travel import show_travels
-from ccp.views.accounts import show_login
-from ccp.views.user import show_user_profile
-# from ccp.views.following import following_handler
